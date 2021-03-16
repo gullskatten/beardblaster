7e9432505a5e8ba8223498646bba36e3bcb73bb7
P <36503627+olaplassen@users.noreply.github.com>
2021-03-16 09:39:50 +0100

    Update core/src/main/kotlin/no/ntnu/beardblaster/view/RegisterScreen.kt
    Co-authored-by: Espen G. <espen_gud@msn.com>

--- core/src/main/kotlin/no/ntnu/beardblaster/view/RegisterScreen.kt
+++ core/src/main/kotlin/no/ntnu/beardblaster/view/RegisterScreen.kt
@@ -63,7 +63,7 @@ class RegisterScreen(game: BeardBlasterGame) : AbstractScreen(game) {
         skin.getDrawable("modal_fancy_header_button_green_check").also { checkButtonStyle.down = it }
         skin.getDrawable("modal_fancy_header_button_red").also { unCheckButtonStyle.up = it }
         skin.getDrawable("modal_fancy_header_button_red_cross").also { unCheckButtonStyle.down = it }
-        skin.getDrawable("button_fancy_dark_short").also { createUserButtonStyle.down = it }
+        skin.getDrawable("button_default_pressed").also { createUserButtonStyle.down = it }
         skin.getDrawable("button_default").also { createUserButtonStyle.up = it }
 
         standardFont.apply {
