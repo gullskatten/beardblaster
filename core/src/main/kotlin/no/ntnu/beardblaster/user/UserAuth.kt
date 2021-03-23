96480bc1ffdde5cd838e3b5b8f4125a036f03fff
andrerim <andreasrim@gmail.com>
2021-03-23 14:36:38 +0100

    Add create firestore user on new authentication user

--- core/src/main/kotlin/no/ntnu/beardblaster/user/UserAuth.kt
+++ core/src/main/kotlin/no/ntnu/beardblaster/user/UserAuth.kt
@@ -3,6 +3,8 @@ package no.ntnu.beardblaster.user
 import ktx.log.debug
 import ktx.log.error
 import ktx.log.logger
+import no.ntnu.beardblaster.commons.User
+import no.ntnu.beardblaster.firestore.Firestore
 import pl.mk5.gdx.fireapp.GdxFIRAuth
 import pl.mk5.gdx.fireapp.auth.GdxFirebaseUser
 
@@ -21,14 +23,17 @@ class UserAuth : IUserAuth {
         GdxFIRAuth.inst()
                 .createUserWithEmailAndPassword(email, password.toCharArray())
                 .then<GdxFirebaseUser> {
-                    LOG.debug { "Created user: $displayName, ${GdxFIRAuth.inst().currentUser?.userInfo?.email}" }
-                    // TODO: Add user to /users/${GdxFIRAuth.inst().currentUser?.userInfo?.uid} with displayName and beardLength = 0
+                    LOG.debug { "Created user: $displayName, ${it.userInfo.email}" }
+
+                    val user = User(displayName, id=it.userInfo.uid)
+                    Firestore<User>().create(user, "users")
                 }
                 .fail { s, _ ->
                     if (s.contains("The email address is already in use by another account")) {
                         LOG.debug { "Tried to create a duplicate user $email" }
                     }
                     LOG.error { "Create user failed $s" }
+                    // TODO: SHOW ERROR TO USER (ON USER CREATION FAIL)
                 }
     }
 
