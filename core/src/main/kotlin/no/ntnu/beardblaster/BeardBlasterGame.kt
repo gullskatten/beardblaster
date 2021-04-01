e02ad487fbbde48a1fadfe5532c11ae5e6149fef
Einar Uvsløkk <einar.uvslokk@gmail.com>
2021-04-01 18:10:46 +0200

    Remove debug log statements

--- core/src/main/kotlin/no/ntnu/beardblaster/BeardBlasterGame.kt
+++ core/src/main/kotlin/no/ntnu/beardblaster/BeardBlasterGame.kt
@@ -8,27 +8,23 @@ import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.inject.Context
 import ktx.inject.register
-import ktx.log.debug
-import ktx.log.logger
 import no.ntnu.beardblaster.screen.*
 
 const val WIDTH = 1920f
 const val HEIGHT = 1080f
 
-private val log = logger<BeardBlasterGame>()
-
 class BeardBlasterGame : KtxGame<KtxScreen>() {
     private val context = Context()
 
     override fun create() {
-        log.debug { "Creating BeardBlaster game" }
         context.register {
             bindSingleton<Batch>(SpriteBatch())
             bindSingleton(AssetManager())
-            bindSingleton(OrthographicCamera().apply { setToOrtho(false, WIDTH, HEIGHT) })
+            bindSingleton(OrthographicCamera().apply {
+                setToOrtho(false, WIDTH, HEIGHT)
+            })
             addScreen(LoadingScreen(this@BeardBlasterGame, inject(), inject(), inject()))
         }
-        log.debug { "Setting LoadingScreen" }
         setScreen<LoadingScreen>()
         super.create()
     }
