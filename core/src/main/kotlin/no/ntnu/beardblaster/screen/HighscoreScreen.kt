d8043b6b0ce13f106133fa153dd40f7d68b7feee
Einar Uvsløkk <einar.uvslokk@gmail.com>
2021-04-05 22:58:42 +0200

    Refactor HighscoreScreen to use default skin style

--- core/src/main/kotlin/no/ntnu/beardblaster/screen/HighscoreScreen.kt
+++ core/src/main/kotlin/no/ntnu/beardblaster/screen/HighscoreScreen.kt
@@ -2,27 +2,20 @@ package no.ntnu.beardblaster.screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-import com.badlogic.gdx.utils.Align
 import ktx.actors.onClick
-import ktx.log.debug
-import ktx.log.logger
+import ktx.scene2d.scene2d
+import ktx.scene2d.table
+import ktx.scene2d.textButton
 import no.ntnu.beardblaster.BeardBlasterGame
 import no.ntnu.beardblaster.HEIGHT
 import no.ntnu.beardblaster.WIDTH
-import no.ntnu.beardblaster.assets.Atlas
-import no.ntnu.beardblaster.assets.Font
-import no.ntnu.beardblaster.assets.get
-
-private val log = logger<HighscoreScreen>()
+import no.ntnu.beardblaster.ui.Image
+import no.ntnu.beardblaster.ui.get
+import no.ntnu.beardblaster.ui.headingLabel
 
 class HighscoreScreen(
     game: BeardBlasterGame,
@@ -30,56 +23,26 @@ class HighscoreScreen(
     assets: AssetManager,
     camera: OrthographicCamera,
 ) : BaseScreen(game, batch, assets, camera) {
-    private val skin = Skin(assets[Atlas.Game])
-    private val font = assets[Font.Standard]
+    private val closeBtn = scene2d.textButton("Close")
 
-    private lateinit var table: Table
-    private lateinit var heading: Label
-
-    private lateinit var closeBtn: TextButton
-
-    private val highscoreStage: Stage by lazy {
+    private val stage: Stage by lazy {
         val result = BeardBlasterStage()
         Gdx.input.inputProcessor = result
         result
     }
 
     override fun show() {
-        log.debug { "HIGHSCORE Screen" }
-
-        table = Table(skin)
-        table.setBounds(0f, 0f, WIDTH, HEIGHT)
-
-        Label.LabelStyle(font, Color.BLACK).also {
-            heading = Label("Leaderbeard", it)
-            heading.setFontScale(2f)
-            it.background = skin.getDrawable("modal_fancy_header")
-            heading.setAlignment(Align.center)
-        }
-
-        val buttonStyle = TextButton.TextButtonStyle()
-        skin.getDrawable("button_default_pressed").also { buttonStyle.down = it }
-        skin.getDrawable("button_default").also { buttonStyle.up = it }
-        font.apply {
-            buttonStyle.font = this
-        }
-
-        closeBtn = TextButton("CLOSE", buttonStyle)
         setBtnEventListeners()
-
-        // Creating table
-        table.apply {
-            this.defaults().pad(30f)
-            this.background = skin.getDrawable("background")
-            this.add(heading)
-            this.row()
-            this.add(closeBtn)
+        val table = scene2d.table {
+            setBounds(0f, 0f, WIDTH, HEIGHT)
+            defaults().pad(30f)
+            background = skin[Image.Background]
+            add(headingLabel("Leaderbeard"))
+            row()
+            add(closeBtn)
         }
-
-        // Adding actors to the stage
-        highscoreStage.addActor(table)
-
-        Gdx.input.inputProcessor = highscoreStage
+        stage.addActor(table)
+        Gdx.input.inputProcessor = stage
     }
 
     override fun update(delta: Float) {}
@@ -95,7 +58,7 @@ class HighscoreScreen(
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         update(delta)
 
-        highscoreStage.act(delta)
-        highscoreStage.draw()
+        stage.act(delta)
+        stage.draw()
     }
 }
