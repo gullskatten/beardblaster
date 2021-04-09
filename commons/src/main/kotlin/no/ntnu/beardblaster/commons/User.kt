ea4ea67a3bf313e87cba44d68047fcaa77fa8cbd
andrerim <andreasrim@gmail.com>
2021-04-09 18:17:04 +0200

    Change bearlength value type to float to fix issue with deserializing

--- commons/src/main/kotlin/no/ntnu/beardblaster/commons/User.kt
+++ commons/src/main/kotlin/no/ntnu/beardblaster/commons/User.kt
@@ -1,6 +1,6 @@
 package no.ntnu.beardblaster.commons
 
 
-data class User(val displayName: String = "Stranger", var beardLength: Number = 0, override var id: String = "") : DocumentType {
+data class User(val displayName: String = "Stranger", var beardLength: Float = 0f, override var id: String = "") : DocumentType {
 
 }
