542935dad2d31337aaf961164ed8a86c6eca1faf
Espen G <espen_gud@msn.com>
2021-04-13 14:22:58 +0200

    Removed unused dependencies.

--- android/src/main/kotlin/no/ntnu/beardblaster/dbclasses/SpellController.kt
+++ android/src/main/kotlin/no/ntnu/beardblaster/dbclasses/SpellController.kt
@@ -1,9 +1,5 @@
 package no.ntnu.beardblaster.dbclasses
 
-import android.content.Context
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import no.ntnu.beardblaster.commons.State
 import no.ntnu.beardblaster.commons.spell.Spell
 import no.ntnu.beardblaster.commons.spell.AbstractSpellRepository
 
